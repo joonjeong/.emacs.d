39edd3d6811a0a5b42a8a9a0372ddf4af733c7c2
Dong-seob Park <ds_j.park@samsung.com>
2014-02-06 04:14:18 -0500

    initialize

---
+++ init.el
@@ -0,0 +1,24 @@
+(setq make-backup-files nil)
+(setq inhibit-splash-screen t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(global-hl-line-mode t)
+(which-function-mode) 
+(ido-mode t)
+ 
+(setq package-archives 
+	'(("gnu" . "http://elpa.gnu.org/packages/")
+	  ("marmalade" . "http://marmalade-repo.org/packages/")
+	  ("melpa" . "http://melpa.milkbox.net/packages/")))
+ 
+(package-initialize)
+ 
+; key mapping
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "<f12>") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+ 
+; key mapping for org-mode
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
