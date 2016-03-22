a26cec3ee8f0073635f799797d69d43fe7d457ac
Dongseob Park <dongseob.park@gmail.com>
2016-03-22 14:37:03 +0900

    add irony

--- init.el
+++ init.el
@@ -65,8 +65,6 @@
   (add-to-list 'company-c-headers-path-system "/usr/include/c++/v1/")
   (add-to-list 'company-c-headers-path-system "/usr/include/c++/4.8/")
   (add-to-list 'company-c-headers-path-system "/usr/include/c++/4.9/"))
-(use-package company-irony :ensure t :pin melpa
-  :config (add-to-list 'company-backends 'company-irony))
 (use-package flycheck :ensure t :pin melpa :config (global-flycheck-mode))
 (use-package flycheck-google-cpplint :ensure t :pin melpa
   :config (flycheck-add-next-checker 'c/c++-clang 'c/c++-googlelint 'append))
@@ -91,6 +89,18 @@
   (setq projectile-enable-caching t)
   (helm-projectile-on))
 (use-package helm-ag :ensure t :pin melpa)
+(use-package irony :ensure t :pin melpa
+  :config
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (defun irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point] 'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol] 'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+(use-package company-irony :ensure t :pin melpa
+  :config (add-to-list 'company-backends 'company-irony))
+(use-package flycheck-irony :ensure t :pin melpa :config (add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 (use-package solarized-theme :ensure t :pin melpa :config (load-theme 'solarized-dark t))
 (use-package undo-tree :ensure t :pin melpa
   :config (global-undo-tree-mode))
