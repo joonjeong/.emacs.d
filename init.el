0188407b510475dbcef9d322c008c64f7182594a
Dongseob Park <dongseob.park@gmail.com>
2016-03-22 13:02:57 +0900

    coloring

--- init.el
+++ init.el
@@ -30,9 +30,27 @@
 (use-package company :ensure t :pin melpa
   :config
   (global-company-mode)
+  (setq company-tooltip-limit 20)
+  (setq company-idle-delay 0)
+  (setq company-echo-delay 0)
+  (setq company-begin-commands '(self-insert-command))
   (setq compan-backends (delete 'company-semantic company-backends))
-  (global-set-key "\t" 'company-complete-common)
-  (define-key company-active-map [tab] 'company-complete-selection)
+  (custom-set-faces
+   '(company-preview
+     ((t (:foreground "darkgray" :underline t))))
+   '(company-preview-common
+     ((t (:inherit company-preview))))
+   '(company-tooltip
+     ((t (:background "lightgray" :foreground "black"))))
+   '(company-tooltip-selection
+     ((t (:background "steelblue" :foreground "white"))))
+   '(company-tooltip-common
+     ((((type x)) (:inherit company-tooltip :weight bold))
+      (t (:inherit company-tooltip))))
+   '(company-tooltip-common-selection
+     ((((type x)) (:inherit company-tooltip-selection :weight bold))
+      (t (:inherit company-tooltip-selection)))))
+  ;(define-key company-active-map [tab] 'company-complete-selection)
   (define-key company-active-map (kbd "TAB") 'company-complete-selection))
 (use-package company-c-headers :ensure t :pin melpa
   :config
